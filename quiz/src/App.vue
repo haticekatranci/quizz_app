a68373464cbc3020cec2e63cc26c593947e61296
hatice <haticekatranci@yandex.com>
2022-12-28 11:45:31 +0300

    add quiz project

--- quiz/src/App.vue
+++ quiz/src/App.vue
@@ -70,25 +70,154 @@ const NextQuestion = () => {
 <template>
  <main class="app">
   <h1> The quiz</h1>
-  <section class="quiz">
-    <div class="quiz-info">
-      <span class="question">{{ getCurrentQuestion.question }}</span>
-      <span class="score">Score {{ score }} / {{ questions.length }}</span>
-    </div>
-  </section>
+   <section class="quiz" v-if="!quizCompleted">
+     <div class="quiz-info">
+       <span class="question">{{ getCurrentQuestion.question }}</span>
+       <span class="score">Score {{ score }}/{{ questions.length }}</span>
+     </div>
+
+     <div class="options">
+       <label
+           v-for="(option, index) in getCurrentQuestion.options"
+           :for="'option' + index"
+           :class="`option ${
+						getCurrentQuestion.selected == index
+							? index == getCurrentQuestion.answer
+								? 'correct'
+								: 'wrong'
+							: ''
+					} ${
+						getCurrentQuestion.selected != null &&
+						index != getCurrentQuestion.selected
+							? 'disabled'
+							: ''
+					}`">
+         <input
+             type="radio"
+             :id="'option' + index"
+             :name="getCurrentQuestion.index"
+             :value="index"
+             v-model="getCurrentQuestion.selected"
+             :disabled="getCurrentQuestion.selected"
+             @change="SetAnswer"
+         />
+         <span>{{ option }}</span>
+       </label>
+     </div>
+
+     <button
+         @click="NextQuestion"
+         :disabled="!getCurrentQuestion.selected">
+       {{
+         getCurrentQuestion.index == questions.length - 1
+             ? 'Finish'
+             : getCurrentQuestion.selected == null
+                 ? 'Select an option'
+                 : 'Next question'
+       }}
+     </button>
+   </section>
+   <section v-else>
+     <h2>You have finished the quiz!</h2>
+     <p>Your score is {{ score }} / {{ questions.length }}</p>
+   </section>
  </main>
 </template>
 
-<style >
+
+<style>
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: 'Monstserrat', sans-serif;
+  font-family: 'Montserrat', sans-serif;
 }
 body {
-  background-color: #271C36;
-  color: #fff;
+  background-color: #271c36;
+  color: #FFF;
 }
-
+.app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem;
+  height: 100vh;
+}
+h1 {
+  font-size: 2rem;
+  margin-bottom: 2rem;
+}
+.quiz {
+  background-color: #382a4b;
+  padding: 1rem;
+  width: 640px;
+}
+.quiz-info {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+}
+.quiz-info .question {
+  color: #8F8F8F;
+  font-size: 1.25rem;
+}
+.quiz-info.score {
+  color: #FFF;
+  font-size: 1.25rem;
+}
+.options {
+  margin-bottom: 1rem;
+}
+.option {
+  padding: 1rem;
+  display: block;
+  background-color: #271c36;
+  margin-bottom: 0.5rem;
+  border-radius: 0.5rem;
+  cursor: pointer;
+}
+.option:hover {
+  background-color: #2d213f;
+}
+.option.correct {
+  background-color: #2cce7d;
+}
+.option.wrong {
+  background-color: #ff5a5f;
+}
+.option:last-of-type {
+  margin-bottom: 0;
+}
+.option.disabled {
+  opacity: 0.5;
+}
+.option input {
+  display: none;
+}
+button {
+  appearance: none;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  padding: 0.5rem 1rem;
+  background-color: #2cce7d;
+  color: #2d213f;
+  font-weight: 700;
+  text-transform: uppercase;
+  font-size: 1.2rem;
+  border-radius: 0.5rem;
+}
+button:disabled {
+  opacity: 0.5;
+}
+h2 {
+  font-size: 2rem;
+  margin-bottom: 2rem;
+  text-align: center;
+}
+p {
+  color: #8F8F8F;
+  font-size: 1.5rem;
+  text-align: center;
+}
 </style>
